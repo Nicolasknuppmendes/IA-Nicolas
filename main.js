d74594620513fcba6f6588c217252985ad2a6701
Nicolasknuppmendes <104834111+Nicolasknuppmendes@users.noreply.github.com>
2024-07-24 19:56:37 +0000

    .

--- main.js
+++ main.js
@@ -3,3 +3,77 @@
  const caixaAlternativas=document.querySelector(".caixa-alternativas")
  const caixaResultados=document.querySelector(".caixa-resultado")
  const textoResultado=document.querySelector(".texto-resultado")
+
+
+ const Perguntas=[
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            { 
+                texto: "Alternativa A",
+                afirmação: "afirmação da alternativa A"
+            },
+            {   
+                texto: "Alternativa B",
+                afirmação: "afirmação da alternativa B"
+            }
+        ]
+    },
+  
+        {
+            enunciado: "Pergunta 2",
+            alternativas: [
+                { 
+                    texto: "Alternativa A",
+                    afirmação: "afirmação da alternativa A"
+                },
+                {   
+                    texto: "Alternativa B",
+                    afirmação: "afirmação da alternativa B"
+                }
+            ]
+        }
+
+             {
+                enunciado: "Pergunta 3",
+                alternativas: [
+                    { 
+                        texto: "Alternativa A",
+                        afirmação: "afirmação da alternativa A"
+                    },
+                    {   
+                        texto: "Alternativa B",
+                        afirmação: "afirmação da alternativa B"
+                    }
+                ]
+            }
+
+            
+                {
+                    enunciado: "Pergunta 4",
+                    alternativas: [
+                        { 
+                            texto: "Alternativa A",
+                            afirmação: "afirmação da alternativa A"
+                        },
+                        {   
+                            texto: "Alternativa B",
+                            afirmação: "afirmação da alternativa B"
+                        }
+                    ]
+                }
+
+                
+                    {
+                        enunciado: "Pergunta 5",
+                        alternativas: [
+                            { 
+                                texto: "Alternativa A",
+                                afirmação: "afirmação da alternativa A"
+                            },
+                            {   
+                                texto: "Alternativa B",
+                                afirmação: "afirmação da alternativa B"
+                            }
+                        ]
+                    }
