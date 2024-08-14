559eee10303657d46585b53387e90e4ab0b3602f
Nicolasknuppmendes <104834111+Nicolasknuppmendes@users.noreply.github.com>
2024-08-14 18:07:06 +0000

    .

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -10,85 +9,69 @@ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            { 
+            {
                 texto: "Alternativa A",
-                afirmação: "afirmação da alternativa A"
+                afirmacao: "afirmacao da alternativa A."
             },
-            {   
+            {
                 texto: "Alternativa B",
-                afirmação: "afirmação da alternativa B"
+                afirmacao: "afirmacao da alternativa B."
             }
         ]
     },
-
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            { 
+            {
                 texto: "Alternativa A",
-                afirmação: "afirmação da alternativa A"
+                afirmacao: "afirmacao da alternativa A."
             },
-            {   
+            {
                 texto: "Alternativa B",
-                afirmação: "afirmação da alternativa B"
-            }
-        ]
-    },
-
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            { 
-                texto: "Alternativa A",
-                afirmação: "afirmação da alternativa A"
-            },
-            {   
-                texto: "Alternativa B",
-                afirmação: "afirmação da alternativa B"
-            }
-        ]
-    },
-
-    {
-        enunciado: "Pergunta 4",
-        alternativas: [
-            { 
-                texto: "Alternativa A",
-                afirmação: "afirmação da alternativa A"
-            },
-            {   
-                texto: "Alternativa B",
-                afirmação: "afirmação da alternativa B"
-            }
-        ]
-    },
-
-    {
-        enunciado: "Pergunta 5",
-        alternativas: [
-            { 
-                texto: "Alternativa A",
-                afirmação: "afirmação da alternativa A"
-            },
-            {   
-                texto: "Alternativa B",
-                afirmação: "afirmação da alternativa B"
+                afirmacao: "afirmacao da alternativa B."
             }
         ]
     }
 ];
 
-let atual= 0;
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostrarPergunta(){
+function mostraPergunta(){
     if (atual >= perguntas.length){
-        mostrarResultado();
+        mostraResultado();t
         return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostrarAlternativas();
+    mostraAlternativas();
 }
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",
+            ()=>respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual ++;
+    mostraPergunta();
+}
+
+
+
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "De acordo com suas respostas...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
